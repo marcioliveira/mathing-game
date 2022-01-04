29d5314d76eb180b28e3aa930d87e4d17cb6e10f
marcioliveira <marcioruelaoliveira@gmail.com>
2022-01-03 21:23:04 -0300

    :construction: Create ArrowDown component without CSS

---
+++ src/components/ArrowDown/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+function ArrowDown() {
+  return /*html*/ `
+        <img src="./images/IconArrowDown.png" alt="Ícone de uma seta para baixo">
+    `;
+}
+
+export default ArrowDown;
